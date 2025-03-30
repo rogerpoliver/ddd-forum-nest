86d249728525c32b08a5fd3d6dd4854785db83ea
Roger Oliveira <rogerpolvr@gmail.com>
2025-03-30 18:09:17 -0300

    feat: add hashed map to create-account controller

--- src/controllers/create-account.controller.ts
+++ src/controllers/create-account.controller.ts
@@ -5,6 +5,7 @@ import {
     HttpCode,
     Post,
 } from "@nestjs/common";
+import { hash } from "bcryptjs";
 import { PrismaService } from "src/prisma/prisma.service";
 
 type Person = {
@@ -32,11 +33,13 @@ export class CreateAccountController {
             throw new ConflictException("Email already exists");
         }
 
+        const hashedPassword = await hash(password, 8);
+
         await this.prisma.user.create({
             data: {
                 name,
                 email,
-                password,
+                password: hashedPassword,
             },
         });
     }
