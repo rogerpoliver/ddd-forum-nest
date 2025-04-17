9c7e1a5f768620634e67e5a5dc8ab8f796d9eae3
Roger Oliveira <rogerpolvr@gmail.com>
2025-04-16 23:47:04 -0300

    feat: add FetchRecentQuestionsController and endpoint for fetching recent questions

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { envSchema } from "../src/env";
 import { ConfigModule } from "@nestjs/config";
 import { AuthenticateController } from "./controllers/authenticate.controller";
 import { CreateQuestionController } from "./controllers/create-question.controller";
+import { FetchRecentQuestionsController } from "./controllers/fetch-recent-questions.controller";
 
 @Module({
 	imports: [
@@ -17,6 +18,7 @@ import { CreateQuestionController } from "./controllers/create-question.controll
 		CreateAccountController,
 		AuthenticateController,
 		CreateQuestionController,
+		FetchRecentQuestionsController,
 	],
 	providers: [PrismaService],
 })
