7b6088aae26c3684ed11b5992d872d7d14ee9357
Roger Oliveira <rogerpolvr@gmail.com>
2025-05-01 20:50:01 -0300

    feat: refactor module structure by moving HTTP-related controllers and services to a new HttpModule

--- src/infra/app.module.ts
+++ src/infra/app.module.ts
@@ -1,12 +1,9 @@
 import { envSchema } from "@/infra/env";
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
-import { AuthModule } from "./auth/auth.module";
-import { AuthenticateController } from "./http/controllers/authenticate.controller";
-import { CreateAccountController } from "./http/controllers/create-account.controller";
-import { CreateQuestionController } from "./http/controllers/create-question.controller";
-import { FetchRecentQuestionsController } from "./http/controllers/fetch-recent-questions.controller";
-import { PrismaService } from "./prisma/prisma.service";
+
+// import { AuthModule } from "./auth/auth.module";
+// import { HttpModule } from "./http/http.module";
 
 @Module({
 	imports: [
@@ -14,14 +11,8 @@ import { PrismaService } from "./prisma/prisma.service";
 			validate: (env) => envSchema.parse(env),
 			isGlobal: true,
 		}),
-		AuthModule,
+		// AuthModule,
+		// HttpModule,
 	],
-	controllers: [
-		CreateAccountController,
-		AuthenticateController,
-		CreateQuestionController,
-		FetchRecentQuestionsController,
-	],
-	providers: [PrismaService],
 })
 export class AppModule {}
