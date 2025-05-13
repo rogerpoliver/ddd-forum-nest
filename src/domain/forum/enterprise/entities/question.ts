1c65a1b68b71be81b9868abd28d95274bd8ad1e9
Roger Oliveira <rogerpolvr@gmail.com>
2025-05-12 21:08:35 -0300

    feat: update Question model and PrismaQuestionMapper to handle bestAnswerId as nullable

--- src/domain/forum/enterprise/entities/question.ts
+++ src/domain/forum/enterprise/entities/question.ts
@@ -8,7 +8,7 @@ import { Slug } from "./value-objects/slug";
 
 export interface QuestionProps {
 	authorId: UniqueEntityID;
-	bestAnswerId?: UniqueEntityID;
+	bestAnswerId?: UniqueEntityID | null;
 	title: string;
 	content: string;
 	slug: Slug;
@@ -79,15 +79,8 @@ export class Question extends AggregateRoot<QuestionProps> {
 		this.touch();
 	}
 
-	set bestAnswerId(bestAnswerId: UniqueEntityID | undefined) {
-		if (bestAnswerId === undefined) {
-			return;
-		}
-
-		if (
-			this.props.bestAnswerId === undefined ||
-			!bestAnswerId.equals(this.props.bestAnswerId)
-		) {
+	set bestAnswerId(bestAnswerId: UniqueEntityID | undefined | null) {
+		if (bestAnswerId && bestAnswerId !== this.props.bestAnswerId) {
 			this.addDomainEvent(
 				new QuestionBestAnswerChosenEvent(this, bestAnswerId),
 			);
